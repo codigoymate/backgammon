07686ece16871260ecbc596aaf3c43592c4fdfb1
Javier Candales <javier_candales@yahoo.com.ar>
2023-08-14 23:11:03 -0300

    document

--- include/backgammon.h
+++ include/backgammon.h
@@ -1,6 +1,19 @@
+/**
+ * @file backgammon.h
+ * @author Javier Candales (javier_candales@yahoo.com.ar)
+ * @brief Main header for Backgammon
+ * @date 2023-08-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #ifndef BACKGAMMON_H
 #define BACKGAMMON_H
 
+/**
+ * @brief Game states
+ */
 #define S_NOT_PLAYING	0
 #define S_ROLL_DICE		1
 #define S_MOVE_PIECES	2
@@ -10,6 +23,10 @@
 #include <board.h>
 #include <player.h>
 
+/**
+ * @brief Main structure of the game
+ * 
+ */
 typedef struct backgammon_t {
 	GtkApplicationWindow *window;
 	GtkLabel *turn_label, *action_label;
@@ -19,16 +36,77 @@ typedef struct backgammon_t {
 	Player player[2];
 } Backgammon;
 
+/**
+ * @brief Creates an instance of backgammon
+ * 
+ * @param argc argument count
+ * @param argv argument vector
+ * @return Backgammon* instance of Backgammon
+ */
 Backgammon *bg_new(int argc, char *argv[]);
+
+/**
+ * @brief Runs backgammon
+ * 
+ * @param bg backgammon instance
+ */
 void bg_run(Backgammon *bg);
 
+/**
+ * @brief Calls the next step. If the game is not in progress, it starts.
+ * Then, the play_func associated with the current player is called.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_next_step(Backgammon *bg);
+
+/**
+ * @brief Switches to the next player's turn.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_next_turn(Backgammon *bg);
+
+/**
+ * @brief Returns the current player.
+ * 
+ * @param bg Backgammon instance
+ * @return Player* instance of the current player
+ */
 Player *bg_current_player(Backgammon *bg);
+
+/**
+ * @brief Returns the opponent of the current player.
+ * 
+ * @param bg Backgammon instance
+ * @return Player* instance of the opponent
+ */
 Player *bg_opponent(Backgammon *bg);
+
+/**
+ * @brief Returns the player who represents the piece(s) at the indicated position.
+ * 
+ * @param bg Backgammon instance
+ * @param data value of the position
+ * @return Player* instance of the player
+ */
 Player *bg_player_by_data(Backgammon *bg, gint data);
 
+/**
+ * @brief Moves a game piece.
+ * 
+ * @param bg Backgammon instance
+ * @param source starting position
+ * @param destiny destination position
+ */
 void bg_move_piece(Backgammon *bg, gint source, gint destiny);
+
+/**
+ * @brief Indicates if the current player can make a move.
+ * 
+ * @param bg Backgammon instance
+ * @return gboolean true if the player can move
+ */
 gboolean bg_player_can_move(Backgammon *bg);
 
 #endif
--- src/backgammon.c
+++ src/backgammon.c
@@ -1,27 +1,57 @@
+/**
+ * @file backgammon.c
+ * @author Javier Candales (javier_candales@yahoo.com.ar)
+ * @brief Implementation of backgammon.h
+ * @date 2023-08-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <backgammon.h>
 #include <utils.h>
 
 #include <player.h>
 
+/**
+ * @brief Frees the Backgammon instance.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_free(Backgammon *bg);
 
+/**
+ * @brief Occurs when the main window is closed.
+ * Frees the Backgammon instance.
+ * 
+ * @param window main window instance
+ * @param data Backgammon instance
+ */
 static void bg_on_window_destroyed(GtkApplicationWindow *window, gpointer data) {
 	bg_free(data);
 }
 
+/**
+ * @brief Occurs when clicking on the "game"->"new game" menu item.
+ * If the game state is S_NOT_PLAYING, ends the current game if the user agrees.
+ * When starting a new game, resets the board and initializes the players.
+ * 
+ * @param menu_item new game menu item
+ * @param data Backgammon instance
+ */
 static void new_game_menu_item_activate(GtkMenuItem *menu_item, gpointer data) {
-	Backgammon *bg = (Backgammon *) data;
+	Backgammon *bg = (Backgammon *)data;
 
 	if (bg->status != S_NOT_PLAYING) {
-		if (!question(GTK_WIDGET(bg->window), "¿Finalizar partida actual?")) return;
+		if (!question(GTK_WIDGET(bg->window), "End the current game?")) return;
 	}
 
 	bg->player[0].play_func = human_play_func;
-	bg->player[0].name = g_string_new("Humano");
+	bg->player[0].name = g_string_new("Human");
 	bg->player[0].piece = BLACK;
 	bg->player[0].direction = 1;
 	bg->player[1].play_func = human_play_func;
-	bg->player[1].name = g_string_new("IA");
+	bg->player[1].name = g_string_new("AI");
 	bg->player[1].piece = WHITE;
 	bg->player[1].direction = -1;
 
@@ -29,24 +59,40 @@ static void new_game_menu_item_activate(GtkMenuItem *menu_item, gpointer data) {
 	board_redraw(bg->board);
 
 	bg_next_step(bg);
-
 }
 
+/**
+ * @brief Occurs when clicking the "Next Turn" button.
+ * Typically associated with the human player. Switches to the next player's turn.
+ * 
+ * @param button button instance
+ * @param data Backgammon instance
+ */
 static void end_turn_button_clicked(GtkWidget *button, gpointer data) {
 	Backgammon *bg;
-	bg = (Backgammon *) data;
+	bg = (Backgammon *)data;
 
 	bg_next_turn(bg);
 	bg_next_step(bg);
 }
 
+/**
+ * @brief Creates an instance of Backgammon.
+ * Initializes the GTK environment.
+ * Creates the board.
+ * Sets the game state to S_NOT_PLAYING.
+ * 
+ * @param argc argument count
+ * @param argv argument vector
+ * @return Backgammon* instance of Backgammon
+ */
 Backgammon *bg_new(int argc, char *argv[]) {
 	Backgammon *bg;
 	GtkBuilder *builder;
 	GtkCssProvider *css_provider;
 	GdkScreen *screen;
 
-	bg = (Backgammon *) g_malloc(sizeof(Backgammon));
+	bg = (Backgammon *)g_malloc(sizeof(Backgammon));
 
 	gtk_init(&argc, &argv);
 
@@ -97,11 +143,23 @@ Backgammon *bg_new(int argc, char *argv[]) {
 	return bg;
 }
 
+/**
+ * @brief Runs Backgammon.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_run(Backgammon *bg) {
 	gtk_widget_show_all(GTK_WIDGET(bg->window));
 	gtk_main();
 }
 
+/**
+ * @brief Frees the Backgammon instance.
+ * Releases Backgammon and Board instances.
+ * Stops the GTK application.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_free(Backgammon *bg) {
 	board_free(bg->board);
 	g_string_free(bg->player[0].name, TRUE);
@@ -110,8 +168,13 @@ void bg_free(Backgammon *bg) {
 	gtk_main_quit();
 }
 
+/**
+ * @brief Calls the next step. If the game is not in progress, it starts.
+ * Then, the play_func associated with the current player is called.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_next_step(Backgammon *bg) {
-
 	if (bg->status == S_NOT_PLAYING) {
 		bg->player_turn = -1;
 		bg_next_turn(bg);
@@ -120,14 +183,20 @@ void bg_next_step(Backgammon *bg) {
 	bg_current_player(bg)->play_func(bg);
 }
 
+/**
+ * @brief Switches to the next player's turn.
+ * Updates the status bar.
+ * Sets the game state to S_ROLL_DICE.
+ * 
+ * @param bg Backgammon instance
+ */
 void bg_next_turn(Backgammon *bg) {
-
 	GString *str;
 
-	bg->player_turn ++;
+	bg->player_turn++;
 	if (bg->player_turn >= 2) bg->player_turn = 0;
 
-	str = g_string_new("Turno de ");
+	str = g_string_new("Turn of ");
 	str = g_string_append(str, bg_current_player(bg)->name->str);
 	gtk_label_set_text(bg->turn_label, str->str);
 
@@ -136,29 +205,62 @@ void bg_next_turn(Backgammon *bg) {
 	bg->status = S_ROLL_DICE;
 }
 
+/**
+ * @brief Returns the current player.
+ * 
+ * @param bg Backgammon instance
+ * @return Player* instance of the current player
+ */
 Player *bg_current_player(Backgammon *bg) {
 	return &bg->player[bg->player_turn];
 }
 
+/**
+ * @brief Returns the opponent of the current player.
+ * 
+ * @param bg Backgammon instance
+ * @return Player* instance of the opponent
+ */
 Player *bg_opponent(Backgammon *bg) {
 	return &bg->player[!bg->player_turn];
 }
 
+/**
+ * @brief Returns the player who represents the piece(s) at the indicated position.
+ * The value of the position represents the player according to the direction:
+ * v < 0: clockwise direction.
+ * v > 0: counterclockwise direction.
+ * The absolute value of the value is the number of pieces at that position.
+ * 
+ * @param bg Backgammon instance
+ * @param data value of the position
+ * @return Player* instance of the player
+ */
 Player *bg_player_by_data(Backgammon *bg, gint data) {
 	Player *player;
 	player = bg_current_player(bg);
 	if (player->direction * data > 0) return player;
-	
+
 	return bg_opponent(bg);
 }
 
+/**
+ * @brief Moves a game piece.
+ * There must be pieces at the source position (from the current player).
+ * Removes one piece from the source and adds it to the destination.
+ * If the destination has an opponent's piece, the current player's prison captures an opponent's piece.
+ * 
+ * @param bg Backgammon instance
+ * @param source starting position
+ * @param destiny destination position
+ */
 void bg_move_piece(Backgammon *bg, gint source, gint destiny) {
 	gint len, i;
-	// Distancia recorrida
+	// Distance traveled
 	len = destiny - source;
 	if (len < 0) len *= -1;
-	// Desactiva el dado según la distancia
-	for (i = 0; i < (bg->board->dice[0] == bg->board->dice[1] ? 4 : 2); i ++) {
+	// Deactivate the die according to the distance
+	for (i = 0; i < (bg->board->dice[0] == bg->board->dice[1] ? 4 : 2); i++) {
 		if (bg->board->consumed_dice[i]) continue;
 		if (bg->board->dice[i % 2] == len) {
 			bg->board->consumed_dice[i] = TRUE;
@@ -166,33 +268,37 @@ void bg_move_piece(Backgammon *bg, gint source, gint destiny) {
 		}
 	}
 
-	
-
-	// Si el destino no es enemigo
+	// If the destination is not an enemy
 	if (bg->board->places[destiny].data * bg_current_player(bg)->direction >= 0) {
-		// Descuenta source
+		// Remove from source
 		bg->board->places[source].data -= bg_current_player(bg)->direction;
 
 		bg->board->places[destiny].data += bg_current_player(bg)->direction;
 	} else {
-		// Si es enemigo ...
+		// If it's an enemy...
 
-		// Coloca la ficha en prision
+		// Put the piece in prison
 		bg->board->prison[bg_current_player(bg)->direction == -1 ? 0 : 1] +=
 				bg_opponent(bg)->direction;
 
-		// Reemplaza la actual
+		// Replace the current piece
 		bg->board->places[destiny].data *= -1;
 	}
 }
 
+/**
+ * @brief Indicates if the current player can make a move.
+ * 
+ * @param bg Backgammon instance
+ * @return gboolean true if the player can move
+ */
 gboolean bg_player_can_move(Backgammon *bg) {
 	gint i;
 	Board *b;
 
 	b = bg->board;
 
-	for (i = 0; i < (b->dice[0] == b->dice[1] ? 4 : 2); i ++) {
+	for (i = 0; i < (b->dice[0] == b->dice[1] ? 4 : 2); i++) {
 		if (!b->consumed_dice[i]) return TRUE;
 	}
 
