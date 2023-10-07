7b207786f4f8066eb1c74313226955dd53f49402
Javier Candales <javier_candales@yahoo.com.ar>
2023-10-07 20:35:57 -0300

    document code

--- include/utils.h
+++ include/utils.h
@@ -1,11 +1,42 @@
+/**
+ * @file utils.h
+ * @author Javier Candales (javier_candales@yahoo.com.ar)
+ * @brief Misc functions module.
+ * @version 0.1
+ * @date 2023-10-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #ifndef UTILS_H
 #define UTILS_H
 
 #include <gtk/gtk.h>
 
+/**
+ * @brief Show yes/no question dialog
+ * 
+ * @param parent Parent window
+ * @param msg The context message
+ * @return gboolean TRUE if "yes" was selected
+ */
 gboolean question(GtkWidget *parent, const char *msg);
+
+/**
+ * @brief Show information dialog
+ * 
+ * @param parent Parent window
+ * @param msg The context message
+ */
 void information(GtkWidget *parent, const char *msg);
 
+/**
+ * @brief Pick a name from ui/names
+ * randomize a name from 100 names file and returns it.
+ * In file: 50 male names and 50 female names.
+ * 
+ * @return GString* the randomize name
+ */
 GString *randomize_name(void);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,22 @@
+/**
+ * @file utils.c
+ * @author Javier Candales (javier_candales@yahoo.com.ar)
+ * @brief implementation of utils.h
+ * @version 0.1
+ * @date 2023-10-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <utils.h>
 
+/**
+ * @brief Show yes/no question dialog
+ * 
+ * @param parent Parent window
+ * @param msg The context message
+ * @return gboolean TRUE if "yes" was selected
+ */
 gboolean question(GtkWidget *parent, const char *msg) {
 
 	GtkWidget *dialog;
@@ -26,6 +43,12 @@ gboolean question(GtkWidget *parent, const char *msg) {
 	return result;
 }
 
+/**
+ * @brief Show information dialog
+ * 
+ * @param parent Parent window
+ * @param msg The context message
+ */
 void information(GtkWidget *parent, const char *msg) {
 	GtkWidget *dialog;
 
@@ -44,6 +67,13 @@ void information(GtkWidget *parent, const char *msg) {
 	gtk_widget_destroy(dialog);
 }
 
+/**
+ * @brief Pick a name from ui/names
+ * randomize a name from 100 names file and returns it.
+ * In file: 50 male names and 50 female names.
+ * 
+ * @return GString* the randomize name
+ */
 GString *randomize_name(void) {
     gchar *file_contents = NULL, **lines, **line;
     gsize file_length = 0;
